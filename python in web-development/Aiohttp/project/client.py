1cf41aa0f2ea0889b65bcc7b2c53547855dec0cd
Ivan Gorobtsov <105990991+yakummi@users.noreply.github.com>
2023-02-26 18:44:15 +0300

    Update client.py

--- python in web-development/Aiohttp/project/client.py
+++ python in web-development/Aiohttp/project/client.py
@@ -3,14 +3,16 @@ from aiohttp import ClientSession
 
 async def main():
     async with ClientSession() as session:
-        response_get = await session.get('http://127.0.0.1:8080/advertisements/',
-                                         json={'advertisement_id': 1})
         response_post = await session.post('http://127.0.0.1:8080/advertisements/',
-                                      json={'description': 'nice desc2',
-                                           'owner': 'yakummi2',
-                                            'header': 'youtube header2',
+                                      json={'description': 'nice fghfhf',
+                                           'owner': 'fgh',
+                                            'header': 'gfhgf header2',
                                            })
-        print(response_get.status)
+        print(response_post.status)
+
+        response_get = await session.get('http://127.0.0.1:8080/advertisements/1/')
+        print(await response_get.json())
+
 
 
 
